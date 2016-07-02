98fd687c04a90220eeaa3b37e52e40e00d7673a9
juhntao <juhntao@live.com>
2016-07-02 10:47:55 +0800

    init TestProject

---
+++ TestProject/ForKey.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject
+{
+    public class ForKey
+    {
+        public ForKey(TableType forKeyTableType, string forKeyPropertyName, IEntity currentEntity)
+        {
+            this.EntityType = forKeyTableType;
+            this.PropertyName = forKeyPropertyName;
+            this.Entity = currentEntity;
+        }
+
+        public TableType EntityType { get; set; }
+
+        public string PropertyName { get; set; }
+
+        public IEntity Entity { get; set; }
+
+        public string GetPropertyValue()
+        {
+            try
+            {
+                return Entity.GetType().GetProperty(PropertyName).GetValue(Entity).ToStringOrNull();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("无法获取属性：" + PropertyName + "，在类型：" + Entity.GetType().ToString() + "中。 详细见内部异常。", ex);
+            }
+        }
+        public void SetPropertyValue(string tid)
+        {
+            try
+            {
+                Entity.GetType().GetProperty(PropertyName).SetValue(Entity, tid);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("无法获取属性：" + PropertyName + "，在类型：" + Entity.GetType().ToString() + "中。 详细见内部异常。", ex);
+            }
+        }
+    }
+
+    public enum TableType
+    {
+        Default
+    }
+
+    public interface IEntity
+    {
+        int Id { get; set; }
+        string Name { get; set; }
+        DateTime CreatedOn { get; set; }
+    }
+
+    public class Entity : IEntity
+    {
+        public int Id { get;set; }
+        public string Name { get; set; }
+        public DateTime CreatedOn { get; set; }
+    }
+
+    public static class ObjctExtend
+    {
+        public static string ToStringOrNull(this object data)
+        {
+            if (data == null)
+            {
+                return null;
+            }
+            return data.ToString();
+        }
+    }
+}
---
+++ TestProject/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var et = new Entity
+            {
+                Id = 10,
+                Name = "juhntao@live.com",
+                CreatedOn = DateTime.Now
+            };
+            var forKey = new ForKey(TableType.Default,"Name",et);
+            Console.WriteLine(forKey.GetPropertyValue());
+            forKey.SetPropertyValue("juhntao");
+            Console.WriteLine(forKey.GetPropertyValue());
+            Console.ReadKey();
+        }
+    }
+}
